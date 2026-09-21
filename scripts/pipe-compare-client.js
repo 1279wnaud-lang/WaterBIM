0a3c3ea699871110b2cb34f46601de7a3e0fa566
user <1279wnaud@gmail.com>
2026-09-21 14:37:58 +0900

    Make pipe headers clickable to view details

--- scripts/pipe-compare-client.js
+++ scripts/pipe-compare-client.js
@@ -37,7 +37,7 @@
  function renderTable(){
   const mats=data.materials.filter(m=>shown.has(m.id));
   const groups=[...new Set(data.rows.map(r=>r.group))];
-  let html='<thead><tr><th scope="col" class="mc-row-head">구분</th>'+mats.map(m=>'<th scope="col"'+(m.recommended?' class="mc-th-pick"':'')+'><div class="mc-th-inner"><div class="mc-th-text">'+esc(m.name)+(m.alias?'<small>'+esc(m.alias)+'</small>':'')+'<span class="mc-std">'+esc(m.standard)+'</span>'+(m.recommended?'<span class="mc-pick-badge">원문 추천</span>':'')+(m.source==='ref'?'<span class="mc-src mc-src-ref">공개 표준</span>':'')+'</div>'+(m.image?'<div class="mc-th-img"><img src="'+esc(m.image)+'" alt=""></div>':'')+'</div></th>').join('')+'</tr></thead>';
+  let html='<thead><tr><th scope="col" class="mc-row-head">구분</th>'+mats.map(m=>'<th scope="col"'+(m.recommended?' class="mc-th-pick"':'')+' style="cursor:pointer;" title="클릭하여 상세 정보 보기" onclick="document.getElementById(\'mc-search-input\').value=\''+m.name+'\'; document.getElementById(\'mc-search-input\').dispatchEvent(new Event(\'input\')); window.scrollTo({top:0, behavior:\'smooth\'});"><div class="mc-th-inner"><div class="mc-th-text">'+esc(m.name)+(m.alias?'<small>'+esc(m.alias)+'</small>':'')+'<span class="mc-std">'+esc(m.standard)+'</span>'+(m.recommended?'<span class="mc-pick-badge">원문 추천</span>':'')+(m.source==='ref'?'<span class="mc-src mc-src-ref">공개 표준</span>':'')+'</div>'+(m.image?'<div class="mc-th-img"><img src="'+esc(m.image)+'" alt=""></div>':'')+'</div></th>').join('')+'</tr></thead>';
   for(const g of groups){
    const rows=data.rows.filter(r=>r.group===g&&mats.some(m=>hasData(m,r)));
    if(!rows.length)continue;
